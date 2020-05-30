54871a074036ff33f51240bff80233a3150c2f16
Aadit Kamat <aadit.k12@gmail.com>
2020-05-30 17:15:26 +0800

    Setup gatsby and remove previous project config (#1)
    * Remove node_modules
    
    * Add Gatsby starter files
    
    * Remove unused directories
    
    * Configure Prettier
    
    * Configure for Heroku deployment
    
    * Update package.json
    
    * Update package-lock.json

---
+++ server/app.js
@@ -0,0 +1,39 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+
+var webHook = require("../routes/webhook");
+
+var app = express().use(bodyParser.json());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/webhook", webHook);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
+
+module.exports = app;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+const Home = () => (
+    <>
+        <h1> Dancetown </h1>
+        <p> Welcome to Dancetown </p>
+    </>
+);
+
+export default Home;
